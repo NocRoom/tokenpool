d9a113aa4e1afbd7610acf265e02cd13ffb6e315
MrCoolLV <22685867+MrCoolLV@users.noreply.github.com>
2018-11-19 11:16:21 -0800

    Update sample.pool.config.js

--- sample.pool.config.js
+++ sample.pool.config.js
@@ -1,11 +1,11 @@
 
 var poolconfig = {
   minimumShareDifficulty: 105,
-  solutionGasPriceWei: 10,
-  transferGasPriceWei: 6,
-  poolTokenFee: 15,
-  minBalanceForTransfer: 1500000000,
-  populationLimit: 100,
+  solutionGasPriceWei: 24,
+  transferGasPriceWei: 22,
+  poolTokenFee: 3,
+  minBalanceForTransfer: 600000000,
+  populationLimit: 1000,
   web3provider: "http://10.0.1.56:8545"
 }
 
