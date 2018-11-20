1ffd99d425038090e4f384e1dfc6268c8bcf5119
MrCoolLV <22685867+MrCoolLV@users.noreply.github.com>
2018-11-20 11:20:47 -0800

    Update sample.account.config.js

--- sample.account.config.js
+++ sample.account.config.js
@@ -2,12 +2,13 @@ var accounts = {
 
   minting: {
     address: '0x8Ca008655f4eCF91577e913B525C909Fd5d1fBf9',
-    privateKey: '00000'
+    privateKey: '',
+    helper: '0x8Ca008655f4eCF91577e913B525C909Fd5d1fBf9'
   },
 
   payment: {
     address: '0x8Ca008655f4eCF91577e913B525C909Fd5d1fBf9',
-    privateKey: '00000'
+    privateKey: ''
   }
 
 }
